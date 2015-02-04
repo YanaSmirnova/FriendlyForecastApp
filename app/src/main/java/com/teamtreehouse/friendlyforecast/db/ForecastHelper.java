a09f7ffd26b54fd74d97fd2c0377197b8c668c6e
YanaSmirnova <smirnova.yana@gmail.com>
2015-02-05 00:09:18 +1100

    Added two main methods and constructor

--- app/src/main/java/com/teamtreehouse/friendlyforecast/db/ForecastHelper.java
+++ app/src/main/java/com/teamtreehouse/friendlyforecast/db/ForecastHelper.java
@@ -1,5 +1,29 @@
 package com.teamtreehouse.friendlyforecast.db;
 
-public class ForecastHelper{
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 
+/**
+ * Class that will extend a base class, and take care of the create operations.
+ */
+
+public class ForecastHelper extends SQLiteOpenHelper{
+
+    private static final String DB_NAME = "temperatures.db";
+    private static final int DB_VERSION = 1;
+
+    public ForecastHelper(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+    // to create the database
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i2) {
+    // to upgrade the database from one version to another
+    }
 }
