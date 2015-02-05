a2ee5bc8ac428b6273b5e9af7b2ecc4b5ea77c5f
YanaSmirnova <smirnova.yana@gmail.com>
2015-02-05 12:33:00 +1100

    Created main variables, constructor and open() method

--- app/src/main/java/com/teamtreehouse/friendlyforecast/db/ForecastDataSource.java
+++ app/src/main/java/com/teamtreehouse/friendlyforecast/db/ForecastDataSource.java
@@ -1,8 +1,35 @@
 package com.teamtreehouse.friendlyforecast.db;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import java.sql.SQLException;
+
 /**
  * Class to act as a an interface on top of the ForecastHelper class and to do all of our operations.
  */
 public class ForecastDataSource {
+    private SQLiteDatabase mDatabase;
+    private ForecastHelper mForecastHelper;
+    private Context mContext;
 
+    public ForecastDataSource (Context context) {
+        mContext = context;
+        mForecastHelper = new ForecastHelper(mContext);
+    }
+
+    // open
+    public void open() throws SQLException {
+        mDatabase = mForecastHelper.getWritableDatabase();
+    }
+
+    // close
+
+    // create
+
+    // insert
+
+    // update
+
+    // delete
 }
