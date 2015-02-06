df46e68c03f7173217f3a9a456be1b543e82e8ba
YanaSmirnova <smirnova.yana@gmail.com>
2015-02-06 22:43:35 +1100

    Triggered upgrade with new version of database, added a column TIME

--- app/src/main/java/com/teamtreehouse/friendlyforecast/db/ForecastHelper.java
+++ app/src/main/java/com/teamtreehouse/friendlyforecast/db/ForecastHelper.java
@@ -13,13 +13,17 @@ public class ForecastHelper extends SQLiteOpenHelper{
     public static final String TABLE_TEMPERATURES = "TEMPERATURES";
     public static final String COLUMN_ID = "_ID";
     public static final String COLUMN_TEMPERATURE = "TEMPERATURE";
+    public static final String COLUMN_TIME = "TIME";
 
     private static final String DB_NAME = "temperatures.db";
-    private static final int DB_VERSION = 1;
+    private static final int DB_VERSION = 2; // must increment to trigger update
     private static final String DB_CREATE =
             "CREATE TABLE " + TABLE_TEMPERATURES + " (" + COLUMN_ID +
                     " INTEGER PRIMARY KEY AUTOINCREMENT" +
-                    ", " + COLUMN_TEMPERATURE + " REAL)";
+                    ", " + COLUMN_TEMPERATURE + " REAL" +
+                    ", " + COLUMN_TIME + " INTEGER)";
+    private static final String DB_ALTER = "ALTER TABLE " + TABLE_TEMPERATURES +
+            " ADD COLUMN " + COLUMN_TIME + " INTEGER";
 
     public ForecastHelper(Context context) {
         super(context, DB_NAME, null, DB_VERSION);
@@ -34,5 +38,6 @@ public class ForecastHelper extends SQLiteOpenHelper{
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i2) {
     // to upgrade the database from one version to another
+        sqLiteDatabase.execSQL(DB_ALTER);
     }
 }
